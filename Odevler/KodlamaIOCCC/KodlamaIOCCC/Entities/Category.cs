f3d79e1c7ecbf2e89c69b418a3fd3e9f55cb8aa7
s.kaanpolat <s.kaanpolat@gmail.com>
2024-02-25 16:00:17 +0300

    Entity classes created

---
+++ Odevler/KodlamaIOCCC/KodlamaIOCCC/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace KodlamaIOCCC.Entities
+{
+	public class Category
+	{
+		public Category()
+		{
+		}
+	}
+}
+
---
+++ Odevler/KodlamaIOCCC/KodlamaIOCCC/Entities/Instructor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace KodlamaIOCCC.Entities
+{
+	public class Instructor
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+	}
+}
+
