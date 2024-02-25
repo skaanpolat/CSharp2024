c809d92bd24ab8f26b9fde2e9317693cf9641722
s.kaanpolat <s.kaanpolat@gmail.com>
2024-02-25 17:50:43 +0300

    Category Manager class filled. Program demo outputs shown on console

--- Odevler/KodlamaIOCCC/KodlamaIOCCC/Business/Concretes/CategoryManager.cs
+++ Odevler/KodlamaIOCCC/KodlamaIOCCC/Business/Concretes/CategoryManager.cs
@@ -1,11 +1,38 @@
 ﻿using System;
+using KodlamaIOCCC.Business.Abstracts;
+using KodlamaIOCCC.DataAccess.Abstracts;
+using KodlamaIOCCC.DataAccess.Concretes;
+using KodlamaIOCCC.Entities;
+
 namespace KodlamaIOCCC.Business.Concretes
 {
-	public class CategoryManager
+	public class CategoryManager : ICategoryService
 	{
-		public CategoryManager()
+        private ICategoryDal _categoryDal;
+		public CategoryManager(CategoryDal categoryDal)
 		{
+            _categoryDal = categoryDal;
 		}
-	}
+
+        public void Add(Category category)
+        {
+            _categoryDal.Add(category);
+        }
+
+        public void Delete(Category category)
+        {
+            _categoryDal.Delete(category);
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return _categoryDal.GetAllCategories();
+        }
+
+        public void Update(Category category)
+        {
+            _categoryDal.Update(category);
+        }
+    }
 }
 
--- Odevler/KodlamaIOCCC/KodlamaIOCCC/Program.cs
+++ Odevler/KodlamaIOCCC/KodlamaIOCCC/Program.cs
@@ -1,7 +1,42 @@
-﻿internal class Program
+﻿using KodlamaIOCCC.Business.Concretes;
+using KodlamaIOCCC.DataAccess.Concretes;
+using KodlamaIOCCC.Entities;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        CategoryManager categoryManager = new CategoryManager(new CategoryDal());
+        var categoryData = categoryManager.GetAllCategories();
+
+        Console.WriteLine("Kategori Listesi\n");
+
+        foreach (var category in categoryData)
+        {
+            Console.WriteLine(category.Name);
+        }
+
+        CourseManager courseManager = new CourseManager(new CourseDal());
+        List<Course> courses = courseManager.GetAllCourses();
+
+        var courseData = courseManager.GetAllCourses();
+
+        Console.WriteLine("-------------------\nDers Listesi\n");
+
+        foreach (var course in courseData)
+        {
+            Console.WriteLine(course.Name);
+        }
+
+        InstructorManager instructorManager = new InstructorManager(new InstructorDal());
+        var instructorData = instructorManager.GetAllInstructors();
+
+        Console.WriteLine("-------------------\nEğitmen Listesi\n");
+
+        foreach (var instructor in instructorData)
+        {
+            Console.WriteLine(instructor.FirstName + " " + instructor.LastName);
+        }
 
     }
 }
